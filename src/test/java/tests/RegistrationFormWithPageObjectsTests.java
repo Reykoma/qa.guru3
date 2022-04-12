b9f9ffd5d8f3320357914d5120e7c49b7e18ac20
nk <nk@MacBook-Air-nk.local>
2022-04-13 00:41:55 +0300

    f2

--- src/test/java/tests/RegistrationFormWithPageObjectsTests.java
+++ src/test/java/tests/RegistrationFormWithPageObjectsTests.java
@@ -18,7 +18,7 @@ public class RegistrationFormWithPageObjectsTests {
     @BeforeAll
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
+        Configuration.browserSize = "1920x1081";
     }
 
     @Test
